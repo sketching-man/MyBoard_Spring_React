e39886a2fd7c11e15b7cc440c9807913477b95ab
Jaemin Lee <dlwoals238@gmail.com>
2022-02-23 02:57:32 +0900

    [Edit] Member domain 변경
    [Edit] Member domain에 undefined ID 수치 추가 / update 함수 구현

--- backend/src/main/java/myboard/spring/domain/Member.java
+++ backend/src/main/java/myboard/spring/domain/Member.java
@@ -5,14 +5,23 @@ import lombok.Data;
 @Data
 public class Member {
 
+    private static final Long initialId = -1L;
+
     private Long id;
     private String userName;
     private String password;
     private Grade grade;
 
     public Member(String userName, String password, Grade grade) {
+        this.id = initialId;
         this.userName = userName;
         this.password = password;
         this.grade = grade;
+    }
+
+    public void update(Member member) {
+        this.userName = member.getUserName();
+        this.password = member.getPassword();
+        this.grade = member.getGrade();
     }
 }
