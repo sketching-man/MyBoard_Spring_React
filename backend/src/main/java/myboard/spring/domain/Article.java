0644105de7a98ba289a44061ecf1039bee02f36a
Jaemin Lee <dlwoals238@gmail.com>
2022-03-03 00:40:14 +0900

    [Edit] Domain 클래스 수정
    [Edit] Domain 클래스에서 Id 관련 인덱싱 설정 및 함수 추가

--- backend/src/main/java/myboard/spring/domain/Article.java
+++ backend/src/main/java/myboard/spring/domain/Article.java
@@ -2,16 +2,17 @@ package myboard.spring.domain;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 public class Article {
 
+    private static final Long defaultId = -1L;
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
@@ -23,11 +24,27 @@ public class Article {
     @Column
     private LocalDateTime writtenTime;
 
+    public Article() {
+        this.id = defaultId;
+        this.title = "";
+        this.body = "";
+        this.writer = new Member();
+        this.writtenTime = LocalDateTime.now();
+    }
+
     public Article(String title, String body, Member writer, LocalDateTime writtenTime) {
+        this.id = defaultId;
         this.title = title;
         this.body = body;
         this.writer = writer;
         this.writtenTime = writtenTime;
+    }
+
+    public void update(Article article) {
+        this.title = article.getTitle();
+        this.body = article.getBody();
+        this.writer = article.getWriter();
+        this.writtenTime = article.getWrittenTime();
     }
 
 }
--- backend/src/main/java/myboard/spring/domain/Member.java
+++ backend/src/main/java/myboard/spring/domain/Member.java
@@ -2,18 +2,34 @@ package myboard.spring.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Member {
 
-    private static final Long initialId = -1L;
+    private static final Long defaultId = -1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String userName;
+    @Column
     private String password;
+    @Column
     private Grade grade;
 
+    public Member() {
+        this.id = defaultId;
+        this.userName = "";
+        this.password = "";
+        this.grade = Grade.User;
+    }
+
     public Member(String userName, String password, Grade grade) {
-        this.id = initialId;
+        this.id = defaultId;
         this.userName = userName;
         this.password = password;
         this.grade = grade;
@@ -24,4 +40,5 @@ public class Member {
         this.password = member.getPassword();
         this.grade = member.getGrade();
     }
+
 }
