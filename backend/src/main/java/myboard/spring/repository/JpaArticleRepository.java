f482c4d0dd9fd19caa26b1184372a7f527b0f2fb
Jaemin Lee <dlwoals238@gmail.com>
2022-07-21 23:51:17 +0900

    [Edit] Article 관련 Jpa Repository 기능 작성
    [Edit] Article 관련 Jpa Repository 기능 작성

--- backend/src/main/java/myboard/spring/repository/JpaArticleRepository.java
+++ backend/src/main/java/myboard/spring/repository/JpaArticleRepository.java
@@ -5,6 +5,7 @@ import myboard.spring.domain.Article;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,52 +17,73 @@ public class JpaArticleRepository implements ArticleRepository {
 
     @Override
     public Article save(Article article) {
-        return null;
+        article.setWrittenTime(LocalDateTime.now());
+        em.persist(article);
+        return article;
     }
 
     @Override
     public boolean existsById(Long id) {
-        return false;
+        return findById(id).isPresent();
     }
 
     @Override
     public Optional<Article> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(em.find(Article.class, id));
     }
 
     @Override
     public List<Article> findAll() {
-        return null;
+        return em.createQuery("SELECT a FROM Article a", Article.class)
+                .getResultList();
     }
 
     @Override
     public List<Article> findByPage(Integer pageNo) {
-        return null;
+        // TODO: 페이지 순서대로 20개씩 잘라서 보여줘야 함. 순서는 작성 시간 기준
+        return em.createQuery("SELECT a FROM Article a", Article.class)
+                .getResultList();
     }
 
     @Override
     public List<Article> findByTitle(String keyword) {
-        return null;
+        // TODO: 순서대로 20개씩 잘라서 보여줘야 함. 순서는 작성 시간 기준
+        String searchPattern = "%" + keyword + "%";
+        return em.createQuery("SELECT a FROM Article a WHERE a.title LIKE :searchPattern", Article.class)
+                .setParameter("searchPattern", searchPattern)
+                .getResultList();
     }
 
     @Override
     public List<Article> findByBody(String keyword) {
-        return null;
+        // TODO: 순서대로 20개씩 잘라서 보여줘야 함. 순서는 작성 시간 기준
+        String searchPattern = "%" + keyword + "%";
+        return em.createQuery("SELECT a FROM Article a WHERE a.body LIKE :searchPattern", Article.class)
+                .setParameter("searchPattern", searchPattern)
+                .getResultList();
     }
 
     @Override
     public List<Article> findByTitleOrBody(String keyword) {
-        return null;
+        // TODO: 순서대로 20개씩 잘라서 보여줘야 함. 순서는 작성 시간 기준
+        String searchPattern = "%" + keyword + "%";
+        return em.createQuery("SELECT a FROM Article a WHERE a.body LIKE :searchPattern", Article.class)
+                .setParameter("searchPattern", searchPattern)
+                .getResultList();
     }
 
     @Override
     public List<Article> findByWriter(String writerName) {
-        return null;
+        // TODO: 순서대로 20개씩 잘라서 보여줘야 함. 순서는 작성 시간 기준
+        return em.createQuery("SELECT a FROM Article a INNER JOIN a.writer w WHERE w.userName = :writerName",
+                        Article.class) // JPQL Inner Join!
+                .setParameter("writerName", writerName)
+                .getResultList();
     }
 
     @Override
     public void deleteById(Long id) {
-
+        findById(id).ifPresent(em::remove);
     }
 
 }
